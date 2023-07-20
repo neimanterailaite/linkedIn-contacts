d37fbd0fc7ab12be7ef6afe7d3b02dbbe0077715
Neimante <neimante.railaite@gmail.com>
2023-07-20 12:37:18 +0200

    adds render contacts

--- script.js
+++ script.js
@@ -25,15 +25,15 @@ fetch(contactUrl8)
   .then((jsonDataAll) => {
     contact8 = jsonDataAll;
     console.log(contact8);
+    renderAllContacts();
   });
 
 //variablen aus HTML ziehen
-const profileBackground = document.querySelector(".profile-background");
-const btnClose = document.querySelector(".btn-close");
+//const profileBackground = document.querySelector(".profile-background");
 
 function renderAllContacts() {
   contact8.forEach(function (contact) {
-    const backgroundImage = document.createElement("img");
+    const backgroundImage = document.createElement("img"); //HTML-Elemente erstellen
     const btnClose = document.createElement("button");
     const profilePicture = document.createElement("img");
     const contactName = document.createElement("h2");
@@ -41,7 +41,32 @@ function renderAllContacts() {
     const connections = document.createElement("p");
     const btnConnect = document.createElement("button");
 
-    backgroundImage.src = contact8.backgroundImage;
-    
+    backgroundImage.setAttribute("src", contact.backgroundImage); //auf das erstellte HTML-Element API-Werte einfügen
+    btnClose.innerText = "x";
+    btnClose.setAttribute("class", "remove-button");
+    profilePicture.setAttribute("src", contact.profilePicture);
+    contactName.innerText =
+      contact.name.title + " " + contact.name.first + " " + contact.name.last;
+    profession.innerText = contact.title;
+    connections.innerText = contact.mutualConnections + " mutual connections";
+    btnConnect.innerText = "Connect";
+
+    const cardWrapper = document.createElement("div");
+    cardWrapper.append(
+      backgroundImage,
+      btnClose,
+      profilePicture,
+      contactName,
+      profession,
+      connections,
+      btnConnect
+    );
+    document.body.appendChild(cardWrapper);
   });
 }
+
+document
+  .querySelector(".remove-button")
+  .addEventListener("click", removeContactCard);
+
+function removeContactCard() {}
